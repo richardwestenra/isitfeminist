f0b3cc23f1d7188fa743a49cff1fa6df29d2e86b
Richard Westenra <rjwestenra@gmail.com>
2016-10-06 17:38:10 +0100

    Refactor things a bit and add transitions etc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,48 @@
 (function(){
-  const question = document.getElementById('question');
-
-  console.log(nouns);
+  const container = document.getElementById('container'),
+    question = container.querySelector('#question'),
+    response = container.querySelector('#response');
 
   function formatQuestion(d) {
     const verb = d[1] ? 'Are' : 'Is',
       noun = d[0];
 
-    return`${verb} ${noun} feminist?`
+    return `${verb} ${noun} feminist?`;
   }
 
-  function getRandomNoun(){
-    return nouns[ Math.floor(Math.random() * nouns.length) ];
+  function getRandom(arr){
+    return arr[ Math.floor(Math.random() * arr.length) ];
+  }
+
+  function toggleSections(showQuestion){
+    question.classList.toggle('hidden', showQuestion);
+    question.setAttribute('aria-hidden', showQuestion.toString());
+    response.classList.toggle('hidden', !showQuestion);
+    response.setAttribute('aria-hidden', (!showQuestion).toString());
   }
 
   function updateQuestion(){
-    question.innerHTML = formatQuestion(getRandomNoun());
+    question.querySelector('h1').innerHTML = formatQuestion(getRandom(nouns));
   }
 
-  window.setInterval(updateQuestion, 1000);
+  function updateResponse(){
+    response.querySelector('h1').innerHTML = getRandom(responses);
+  }
+
+  updateQuestion();
+
+  question.addEventListener('click', function() {
+    if (event.target.localName === 'button') {
+      updateResponse();
+      toggleSections(true);
+    }
+  });
+
+  response.addEventListener('click', function() {
+    if (event.target.localName === 'button') {
+      updateQuestion();
+      toggleSections(false);
+    }
+  });
 
 })();
