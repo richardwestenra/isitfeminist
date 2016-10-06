c52475ed6525f33db3e800545373154886baad62
Richard Westenra <rjwestenra@gmail.com>
2016-10-06 10:33:45 +0100

    Add initial noun list and make a start on the JS & styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+(function(){
+  const question = document.getElementById('question');
+
+  console.log(nouns);
+
+  function formatQuestion(d) {
+    const verb = d[1] ? 'Are' : 'Is',
+      noun = d[0];
+
+    return`${verb} ${noun} feminist?`
+  }
+
+  function getRandomNoun(){
+    return nouns[ Math.floor(Math.random() * nouns.length) ];
+  }
+
+  function updateQuestion(){
+    question.innerHTML = formatQuestion(getRandomNoun());
+  }
+
+  window.setInterval(updateQuestion, 1000);
+
+})();
---
+++ app/scripts/nouns.js
@@ -0,0 +1,8 @@
+const nouns = [
+['bananas',1],
+['feminists',1],
+['web development',0],
+['sugar',0],
+['magazines',1],
+['foozball',0],
+];
